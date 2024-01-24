0074b0092d01210674f9568bef858bbca9d8f8b5
aa953721 <aabdelqader@ohio.edu>
2024-01-23 20:08:32 -0500

    commit

---
+++ git-lab-program.cc
@@ -0,0 +1,19 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: Ahmad Abdelqader
+ *        Date: 1/23/24
+ * Description: My git lab program
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+    
+    cout << "Hello Git!!" << endl;
+
+    return 0;
+}// main
